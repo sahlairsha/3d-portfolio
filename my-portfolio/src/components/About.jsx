f201c61eb21f3a6628f587b73e029e7b573d36c6
sahlairsha <fsahla370@gmail.com>
2023-10-16 15:59:47 +0530

    change

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
       <h2 className={styles.sectionHeadText}>Overview.</h2>
     </motion.div>
     <motion.p variants={fadeIn("","", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-      I'm a skilled Front-end developer with experirnce in JavaScript,Html5,Css and experience in framework like React.js,Node.js. 
+      I'm a skilled Front-end developer withexperience in JavaScript,Html5,Css and experience in framework like React.js,Node.js. 
       I'm a qucik learner and collaborate closely with clients to create efficient,scalable and user-friendly solutions that solve real world problems.
       Let's work together to bring your ideas to life.
     </motion.p>
